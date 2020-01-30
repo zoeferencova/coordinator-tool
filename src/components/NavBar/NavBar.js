2b971d9c71455000ea44d3ff512a53a30d30638a
Zoe Ferencova <zoeferencova@gmail.com>
2020-01-30 13:14:20 -0500

    made nav bar responsive

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,50 +3,69 @@ import { NavLink } from 'react-router-dom';
 import './NavBar.css'
 
 export default class NavBar extends React.Component {
-    
+    state = {
+        expanded: false
+    }
+
+    showMobile() {
+        if (window.innerWidth >= 768) {
+            if (this.state.expanded) {
+                return 'show'
+            } else {
+                return 'hidden'
+            }
+        }
+        return 'show'
+    }
+
     render() {
-        return (      
-            <nav role="navigation">
-                <h1 className="NavBar__logo">Logo</h1>
-                <ul className="tab-list">
-                    <li className="tab">
-                        <NavLink to="/main">
-                            <i className="fas fa-list-alt"></i>
-                            <span className="tab-title">List</span>
+        return (   
+            <div className='nav-wrap'>
+                <button className={`burger`} onClick={() => this.setState({ expanded: true })}><i className="fas fa-bars"></i></button>
+                <nav role="navigation" className={`App__nav ${this.state.expanded ? 'show': ''}`} >
+                    <h1 className="NavBar__logo">Logo</h1>
+                    <ul className="tab-list">
+                        <NavLink to="/main" className="tab">
+                            <li className="tab-content">
+                                <i className="fas fa-list-alt"></i>
+                                <span className="tab-title">List</span>
+                            </li>
                         </NavLink>
-                    </li>
-                    <li className="tab">
-                        <NavLink to="/completed">
-                            <i className="fas fa-check-square"></i>
-                            <span className="tab-title">Completed</span>
+                        <NavLink to="/completed" className="tab">
+                            <li className="tab-content">
+                                <i className="fas fa-check-square"></i>
+                                <span className="tab-title">Completed</span>
+                            </li>
                         </NavLink>
-                    </li>
-                    <li className="tab">
-                        <NavLink to="/archive">
-                            <i className="fas fa-archive"></i>
-                            <span className="tab-title">Archive</span>
+                        <NavLink to="/archive" className="tab">
+                            <li className="tab-content">
+                                <i className="fas fa-archive"></i>
+                                <span className="tab-title">Archive</span>
+                            </li>
+                        </NavLink>                                       
+                        <NavLink to="/dashboard" className="tab">
+                            <li className="tab-content">
+                                <i className="fas fa-chart-pie"></i>
+                                <span className="tab-title">Dashboard</span>
+                            </li>
                         </NavLink>
-                    </li>
-                    <li className="tab">
-                        <NavLink to="/dashboard">
-                            <i className="fas fa-chart-pie"></i>
-                            <span className="tab-title">Dashboard</span>
+                        <NavLink to="email" className="tab">
+                            <li className="tab-content">                       
+                                <i className="fas fa-envelope"></i>
+                                <span className="tab-title">Templates</span>                        
+                            </li>
                         </NavLink>
-                    </li>
-                    <li className="tab">
-                        <NavLink to="email">
-                            <i className="fas fa-envelope"></i>
-                            <span className="tab-title">Templates</span>
+                        <NavLink to="account" className="tab">
+                            <li className="tab-content">
+                                <i className="fas fa-user-circle"></i>
+                                <span className="tab-title">Account</span>
+                            </li>
                         </NavLink>
-                    </li>
-                    <li className="tab">
-                        <NavLink to="account">
-                            <i className="fas fa-user-circle"></i>
-                            <span className="tab-title">Account</span>
-                        </NavLink>
-                    </li>
-                </ul>
-            </nav>
+                    </ul>
+                </nav>
+                <div className={`overlay ${this.state.expanded ? 'show' : 'hidden'}` } onClick={() => this.setState({ expanded: false })}></div>
+            </div>   
+            
         )
     }
 }
