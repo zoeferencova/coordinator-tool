a145f36330ae369fde2fb40c8d0048a3fdcba048
Zoe Ferencova <zoeferencova@gmail.com>
2020-01-31 12:53:22 -0500

    added status icons to desktop version of mainl ist

--- src/components/MainListItem/MainListItem.js
+++ src/components/MainListItem/MainListItem.js
@@ -1,18 +1,38 @@
 import React from 'react';
 import './MainListItem.css'
 
+const icons = {
+    'none': <i className="far fa-circle"></i>,
+    'reached': <i className="fas fa-arrow-circle-left"></i>,
+    'completed': <i className="fas fa-check-circle"></i>
+}
+
 export default class MainListItem extends React.Component {
-    
+    state = {
+        status: 'none',
+        expanded: false
+    }
+
+    handleStatusClick(status) {
+        this.setState({status});
+        if (this.state.expanded === true) {
+            this.setState({ expanded: false })
+        } else {
+            this.setState({ expanded: true })
+        }
+    }
+
     render() {
         return (
             <div className="table-row row">
                 <div className="table-body-cell"><input type="checkbox"></input></div>
                 <div className="table-body-cell">
-                    <select name="status" id="status">
-                        <option value="none"></option>
-                        <option value="reached">Reached Out</option>
-                        <option value="completed">Completed</option>
-                    </select>
+                    <span className="status-icon" onClick={() => this.state.expanded === true ? this.setState({ expanded: false }) : this.setState({ expanded: true })}>{icons[this.state.status]}</span>
+                    <ul className={`icon-list hide-list ${this.state.expanded && 'show-list'}`}>
+                        <li onClick={() => this.handleStatusClick('none')}>{icons['none']} None</li>
+                        <li onClick={() => this.handleStatusClick('reached')}>{icons['reached']} Reached Out</li>
+                        <li className='last' onClick={() => this.handleStatusClick('completed')}>{icons['completed']} Completed</li>
+                    </ul>
                 </div>
                 <div className="table-body-cell proj-cell">{this.props.project}</div>
                 <div className="table-body-cell adv-cell">{this.props.advisor}</div>
