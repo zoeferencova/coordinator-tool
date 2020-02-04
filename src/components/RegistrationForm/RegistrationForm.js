4831841d1186302678bcd8c786d0055d09bb38b2
zoeferencova <zoeferencova@gmail.com>
2020-02-04 15:37:47 -0500

    updated registration form to include method for submission

--- src/components/RegistrationForm/RegistrationForm.js
+++ src/components/RegistrationForm/RegistrationForm.js
@@ -1,25 +1,58 @@
 import React from 'react';
+import AuthApiService from '../../services/auth-api-service';
 
 export default class RegistrationForm extends React.Component {
+    static defaultProps = {
+        onRegistrationSuccess: () => {}
+    }
+
+    state = { error: null };
+
+    handleSubmit = e => {
+        e.preventDefault();
+        const { full_name, email, password } = e.target;
+
+        this.setState({ error: null })
+
+        AuthApiService.postUser({
+            full_name: full_name.value,
+            email: email.value,
+            password: password.value
+        })
+            .then(user => {
+                full_name.value = '';
+                email.value = '';
+                password.value = '';
+                this.props.onRegistrationSuccess();
+            })
+            .catch(res => {
+                this.setState({ error: res.error })
+            })
+    }
+    
     render() {
+        const { error } = this.state;
         return (
             <main>
-                <form>
-                    <div>
-                        <label for="first-name">First name</label>
-                        <input placeholder='First Name' type="text" name='first-name' id='first-name' />
+                <form onSubmit={this.handleSubmit}>
+                    <div role="alert">
+                        {error && <p>{error}</p>}
                     </div>
                     <div>
-                        <label for="last-name">Last name</label>
-                        <input type="text" name='last-name' id='last-name' placeholder='Last Name' />
+                        <label htmlFor="first-name">First name</label>
+                        <input required placeholder='First Name' type="text" name='first-name' id='first-name' />
                     </div>
                     <div>
-                        <label for="username">Email</label>
-                        <input type="text" name='username' id='username' />
+                        <label htmlFor="last-name">Last name</label>
+                        <input required type="text" name='last-name' id='last-name' placeholder='Last Name' />
                     </div>
                     <div>
-                        <label for="password">Password</label>
-                        <input type="password" name='password' id='password' />
+                        <label htmlFor="username">Email</label>
+                        <input required type="text" name='username' id='username' />
+                    </div>
+                    <div>
+                        <label htmlFor="password">Password</label>
+                        <input required type="password" name='password' id='password' />
                     </div>
                     <button>Cancel</button>
                     <button type='submit'>Create Account</button>
--- src/routes/RegistrationPage/RegistrationPage.js
+++ src/routes/RegistrationPage/RegistrationPage.js
@@ -4,11 +4,24 @@ import RegistrationForm from '../../components/RegistrationForm/RegistrationForm
 import './RegistrationPage.css'
 
 export default class RegistrationPage extends React.Component {
+    static defaultProps = {
+        history: {
+            push: () => {},
+        },
+    }
+
+    handleRegistrationSuccess = user => {
+        const { history } = this.props;
+        history.push('/login')
+    }
+    
     render() {
         return (
             <main role="main">
                 <h1>Sign up</h1>
-                <RegistrationForm />
+                <RegistrationForm
+                    onRegistrationSuccess={this.handleRegistrationSuccess}
+                />
                 <p>Already have an account? <Link to='/login'>Log in</Link></p>
                 <p>Don't work at GP? <Link to='/main'>See the demo</Link></p>
             </main>
