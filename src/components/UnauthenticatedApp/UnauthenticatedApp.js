25f1b7c8423871dfc0fcfaf9c78dfe0c8a5b901b
zoeferencova <zoeferencova@gmail.com>
2020-02-19 14:29:00 -0500

    added selective rendering of authenticated and unauthenticated app versions for login

---
+++ src/components/UnauthenticatedApp/UnauthenticatedApp.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import LandingPage from '../../routes/LandingPage/LandingPage';
+import LoginPage from '../../routes/LoginPage/LoginPage';
+import RegistrationPage from '../../routes/RegistrationPage/RegistrationPage';
+import NotFoundPage from '../../routes/NotFoundPage/NotFoundPage';
+
+import './UnauthenticatedApp.css';
+
+export default class UnauthenticatedApp extends Component {
+
+  render() {
+    return ( 
+        <main className='App'>
+          <Switch>
+            <Route exact path={'/'} component={LandingPage} />
+            <Route path={'/login'} component={() => <LoginPage setLoggedIn={this.props.setLoggedIn} />} />
+            <Route path={'/register'} component={RegistrationPage} />
+            <Route component={NotFoundPage} />
+          </Switch>
+        </main>
+    );
+  }
+}
