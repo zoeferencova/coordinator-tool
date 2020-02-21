910ed4a864e3c7c73ccae2bc691f5ea68a51029f
zoeferencova <zoeferencova@gmail.com>
2020-02-21 17:38:14 -0500

    dashboard completion time fix

--- src/routes/DashboardPage/DashboardPage.js
+++ src/routes/DashboardPage/DashboardPage.js
@@ -31,17 +31,26 @@ export default class DashboardPage extends React.Component {
 
     getMinutes(item) {
         let minutes = 0;
-        if (item.difference.days) {
-            minutes += (item.difference.days*24*60 + item.difference.hours*60 + item.difference.minutes)
-        } else if (!item.difference.days && item.difference.hours) {
-            minutes += (item.difference.hours*60 + item.difference.minutes)
-        } else if (!item.difference.hours && item.difference.minutes) {
-            minutes += item.difference.minutes
-        } else if (!item.difference.minutes && item.difference.seconds) {
-            minutes += 1
+        if (item.difference.months) {
+            minutes += item.difference.months*4*7*24*60 
         }
-    
-        item = minutes;
+        if (item.difference.weeks) {
+            minutes += item.difference.weeks*7*24*60
+        }
+        if (item.difference.days) {
+            minutes += item.difference.days*24*60 
+        }
+        if (item.difference.hours) {
+            minutes += item.difference.hours*60
+        }
+        if (item.difference.minutes) {
+            minutes += item.difference.minutes
+        }
+        if (item.difference.seconds) {
+            minutes += 0
+        }
+        item = minutes
+
         return item
     }
     
