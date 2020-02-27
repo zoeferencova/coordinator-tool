cb71fb5cf71c8bf8207a135a03f3596b72dc2d88
zoeferencova <zoeferencova@gmail.com>
2020-02-26 21:00:11 -0500

    basic formatting for email templates

--- src/components/EmailTemplate/EmailTemplate.js
+++ src/components/EmailTemplate/EmailTemplate.js
@@ -8,105 +8,12 @@ import Button from '../Utils/Utils';
 export default class EmailTemplate extends React.Component {
     static contextType = AppContext;
     
-    state = {
-        expanded: false,
-        edit: false,
-        inputValues: {}
-    }
-
-    handleDeleteTemplate(e) {
-        e.preventDefault();
-        const templateId = ReactDOM.findDOMNode(e.target).parentNode.getAttribute('itemkey')
-        console.log(templateId)
-        fetch(`${config.API_ENDPOINT}/templates/${templateId}`, {
-            method: 'DELETE',
-            headers: {
-                'content-type': 'application/json',
-                'Authorization': `Bearer ${window.sessionStorage.getItem(config.TOKEN_KEY)}`
-            }
-        })
-        this.context.deleteTemplate(templateId)
-    }
-
-    handlePatchTemplate(e) {
-        e.preventDefault()
-        fetch(`${config.API_ENDPOINT}/templates/${this.props.id}`, {
-            method: 'PATCH',
-            headers: {
-                'content-type': 'application/json',
-                'Authorization': `Bearer ${window.sessionStorage.getItem(config.TOKEN_KEY)}`
-            },
-            body: JSON.stringify(this.state.inputValues)
-        })
-            .then(res => this.context.updateTemplate(this.state.inputValues))
-            .then(this.setState({ edit: false, expanded: false }))
-    }
-
-    componentDidMount() {
-        const templateId = this.props.id;
-        
-        fetch(`${config.API_ENDPOINT}/templates/${templateId}`, {
-            method: 'GET',
-            headers: {
-                'content-type': 'application/json',
-                'Authorization': `Bearer ${window.sessionStorage.getItem(config.TOKEN_KEY)}`
-            }
-        })
-            .then(res => res.json())
-            .then(resJson => this.setState({ inputValues: resJson }))
-    }
-
-    handleEmailEdit = () => {
-        this.setState({ edit: true })
-    }
-
-    closeEmailEdit = () => {
-        this.setState({ edit: false })
-    }
-
-    handleChangeName = val => {
-        this.setState({ inputValues: { ...this.state.inputValues, template_name: val } })
-    }
-
-    handleChangeSubject = val => {
-        this.setState({ inputValues: {...this.state.inputValues, template_subject: val } })
-    }
-
-    handleChangeContent = val => {
-        this.setState({ inputValues: { ...this.state.inputValues, template_content: val } })
-    }
-    
     render() {
         return (
             <div>
-                <div className={styles.templateItem} onClick={() => this.state.expanded ? this.setState({ expanded: false, edit: false }) : this.setState({ expanded: true })}>
-                    <h2 >{this.props.template_name}</h2>
+                <div className={styles.templateItem} onClick={() => this.props.selectTemplate(this.props.id)}>
+                    {this.props.template_name}
                 </div>
-                {this.state.expanded && !this.state.edit && <div className={styles.templateContent} itemkey={this.props.id}>
-                    <p>Subject: {this.props.template_subject}</p>
-                    <p className={styles.templateBody}>Body: {this.props.template_content}</p>
-                    <Button onClick={this.handleEmailEdit}>Edit</Button>
-                    <Button onClick={e => this.handleDeleteTemplate(e)}>Delete</Button>
-                </div>}
-                {this.state.expanded && this.state.edit && <form onSubmit={e => this.handlePatchTemplate(e)}>
-                        <div>
-                            <label htmlFor="template-name">Template Name: </label>
-                            <input type="text" name='template-name' id='template-name' defaultValue={this.state.inputValues.template_name} onChange={e => this.handleChangeName(e.target.value)}></input>
-                        </div>
-                        <div>
-                            <label htmlFor="template-subject">Subject: </label>
-                            <input type="text" name='template-subject' id='template-subject' defaultValue={this.state.inputValues.template_subject} onChange={e => this.handleChangeSubject(e.target.value)}></input>
-                        </div>
-                        <br></br>
-                        <div>
-                            <label htmlFor="template-body">Body: </label>
-                            <textarea name="template-body" id="template-body" cols="100" rows="30" defaultValue={this.state.inputValues.template_content} onChange={e => this.handleChangeContent(e.target.value)}></textarea>
-                        </div>
-                        <div>
-                            <Button onClick={this.closeEmailEdit}>Cancel</Button>
-                            <Button type='submit'>Save</Button>
-                        </div>
-                    </form>}
             </div>
             
         )
--- src/routes/EmailPage/EmailPage.js
+++ src/routes/EmailPage/EmailPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import NavBar from '../../components/NavBar/NavBar';
 import EmailTemplate from '../../components/EmailTemplate/EmailTemplate'
+import EmailTemplateWindow from '../../components/EmailTemplateWindow/EmailTemplateWindow'
 import Header from '../../components/Header/Header'
 import AppContext from '../../contexts/contexts'
 import Button from '../../components/Utils/Utils'
@@ -11,7 +12,12 @@ import styles from './EmailPage.module.css'
 export default class EmailPage extends React.Component {
     static contextType = AppContext;
 
-    renderTemplates() {
+    state = {
+        currentTemplate: this.context.templates[0],
+    }
+    
+
+    renderTemplateTabs() {
         return this.context.templates.map(template => 
             <EmailTemplate
                 key={template.id}
@@ -19,6 +25,24 @@ export default class EmailPage extends React.Component {
                 template_name={template.template_name}
                 template_subject={template.template_subject}
                 template_content={template.template_content}
+                selectTemplate={this.selectTemplate}
+            />
+        )
+    }
+
+    selectTemplate = id => {
+        const currentTemplate = this.context.templates.find(template => template.id === id)
+        this.setState({ currentTemplate })
+    }
+
+    renderTemplateContent() {
+        const { id, template_name, template_subject, template_content } = this.state.currentTemplate;
+        return (
+            <EmailTemplateWindow 
+                id={id}
+                template_name={template_name}
+                template_subject={template_subject}
+                template_content={template_content}
             />
         )
     }
@@ -28,9 +52,17 @@ export default class EmailPage extends React.Component {
             <div className="container">
                 <main className="content">
                     <Header title={'Email Templates'} />
-                    {this.renderTemplates()}<br></br>
-                    <Link to='/new-template'><Button>New Template</Button></Link>
-                    
+                    <div className={styles.pageContainer}>
+                        <div className={styles.templateContainer}>
+                            <div className={styles.templateList}>
+                                {this.renderTemplateTabs()}
+                            </div>
+                            <div className={styles.templateWindow}>
+                                {this.state.currentTemplate !== undefined && this.renderTemplateContent()}
+                            </div>
+                        </div>
+                        <Link to='/new-template'><Button>New Template</Button></Link>
+                    </div>
                 </main>
                 <NavBar className="nav" />
             </div>
