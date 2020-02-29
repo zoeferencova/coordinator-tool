aa6382e9e8f8788b6876345dd9522b7661fdb7c1
zoeferencova <zoeferencova@gmail.com>
2020-02-29 14:50:48 -0500

    finished landing page and added favicon

--- src/components/PieChart/PieChart.js
+++ src/components/PieChart/PieChart.js
@@ -25,76 +25,81 @@ export default class PieChart {
 
         d3.json(`${config.API_ENDPOINT}/data/pm-data`, { headers: { "Authorization": `Bearer ${window.sessionStorage.getItem(config.TOKEN_KEY)}` } })
             .then(res => {
-                const colors = colorbrewer.Spectral[res.length]
-                const arr = []
-                res.forEach(e => arr.push(parseInt(e.count)))
-                const total = arr.reduce((acc, el) => acc + el)
-                res.map((d, i) => d.color = colors[i])
-                res.sort((a, b) => b.count - a.count)
-                vis.data = res;
-                vis.color = d3.scaleOrdinal()
-                    .domain(vis.data.map(d => d.pm_name))
-                    .range(vis.data.map(d => d.color))
+                if (res.length !== 0) {
+                    const colors = colorbrewer.Spectral[res.length]
+                    const arr = []
+                    res.forEach(e => arr.push(parseInt(e.count)))
+                    const total = arr.reduce((acc, el) => acc + el)
+                    res.map((d, i) => d.color = colors[i])
+                    res.sort((a, b) => b.count - a.count)
+                    vis.data = res;
+                    vis.color = d3.scaleOrdinal()
+                        .domain(vis.data.map(d => d.pm_name))
+                        .range(vis.data.map(d => d.color))
 
-                vis.pie = d3.pie()
-                    .value(d => d.value.count)
+                    vis.pie = d3.pie()
+                        .value(d => d.value.count)
 
-                vis.data_ready = vis.pie(d3.entries(vis.data))
+                    vis.data_ready = vis.pie(d3.entries(vis.data))
 
-                vis.info = d3.select(element)
-                    .append("div")
-                    .attr("class", "info")
-                    .style("opacity", 0)
+                    vis.info = d3.select(element)
+                        .append("div")
+                        .attr("class", "info")
+                        .style("opacity", 0)
 
-                vis.svg
-                    .selectAll('whatever')
-                    .data(vis.data_ready)
-                    .enter()
-                    .append('path')
-                    .attr('d', d3.arc()
-                        .innerRadius(INNERRADIUS)         // This is the size of the donut hole
-                        .outerRadius(RADIUS)
-                    )
-                    .attr('fill', function(d){ return(vis.color(d.data.key)) })
-                    .style("stroke-width", "2px")
-                    .style("opacity", 0.7)
-                    .on("mouseover", function(d) {
-                        d3.select(this)
-                            .style("cursor", "pointer")
-                            .transition()
-                            .duration(300)
-                            .attr("d", d3.arc().innerRadius(INNERRADIUS).outerRadius(RADIUS + 10));
-                        d3.select(info)
-                            .style("opacity", 1)
-                            .append("text")
-                                .attr("class", "text")
-                                .text(`${d.data.value.pm_name} - ${Math.round((d.data.value.count*100)/total)}%`)
-                      })
-                      .on("mouseout", function(d) {
-                        d3.select(this).transition()
-                          .duration(500)
-                          .attr("d", d3.arc().innerRadius(INNERRADIUS).outerRadius(RADIUS))
+                    vis.svg
+                        .selectAll('whatever')
+                        .data(vis.data_ready)
+                        .enter()
+                        .append('path')
+                        .attr('d', d3.arc()
+                            .innerRadius(INNERRADIUS)         // This is the size of the donut hole
+                            .outerRadius(RADIUS)
+                        )
+                        .attr('fill', function(d){ return(vis.color(d.data.key)) })
+                        .style("stroke-width", "2px")
+                        .style("opacity", 0.7)
+                        .on("mouseover", function(d) {
+                            d3.select(this)
+                                .style("cursor", "pointer")
+                                .transition()
+                                .duration(300)
+                                .attr("d", d3.arc().innerRadius(INNERRADIUS).outerRadius(RADIUS + 10));
+                            d3.select(info)
+                                .style("opacity", 1)
+                                .append("text")
+                                    .attr("class", "text")
+                                    .text(`${d.data.value.pm_name} - ${Math.round((d.data.value.count*100)/total)}%`)
+                        })
+                        .on("mouseout", function(d) {
+                            d3.select(this).transition()
+                            .duration(500)
+                            .attr("d", d3.arc().innerRadius(INNERRADIUS).outerRadius(RADIUS))
 
-                        d3.select(info)
-                            .style("opacity", 0)
-                            .select(".text")
-                            .remove()
-                      })
+                            d3.select(info)
+                                .style("opacity", 0)
+                                .select(".text")
+                                .remove()
+                        })
 
-                vis.svg.append("g")
-                    .attr("class", "legendOrdinal")
-                    .attr("transform", "translate(150, -100)")
-                    .attr("font-size", 12)
+                    vis.svg.append("g")
+                        .attr("class", "legendOrdinal")
+                        .attr("transform", "translate(150, -100)")
+                        .attr("font-size", 12)
 
-                const legendOrdinal = legendColor()
-                    .labelFormat(d3.format(".2f"))
-                    .shape("path", d3.symbol().type(d3.symbolCircle).size(150)())
-                    .shapePadding(10)
-                    .cellFilter(d => isNaN(Number(d.label)))
-                    .scale(vis.color)
+                    const legendOrdinal = legendColor()
+                        .labelFormat(d3.format(".2f"))
+                        .shape("path", d3.symbol().type(d3.symbolCircle).size(150)())
+                        .shapePadding(10)
+                        .cellFilter(d => isNaN(Number(d.label)))
+                        .scale(vis.color)
 
-                vis.svg.select(".legendOrdinal")
-                    .call(legendOrdinal)
+                    vis.svg.select(".legendOrdinal")
+                        .call(legendOrdinal)
+                } else {
+                    return 'No data yet'
+                }
+                
 
             })
         
