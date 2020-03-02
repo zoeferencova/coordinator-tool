1f173cfa576cdd0e56a41ccbe8768fa401f09e85
Zoe Ferencova <zoeferencova@gmail.com>
2020-03-02 13:36:37 -0500

    added user guide

--- src/routes/AccountPage/AccountPage.js
+++ src/routes/AccountPage/AccountPage.js
@@ -8,6 +8,8 @@ import { Button, Input } from '../../components/Utils/Utils'
 import config from '../../config'
 
 import styles from './AccountPage.module.css'
+import modalStyles from '../../components/SendEmailForm/SendEmailForm.module.css'
+import listStyles from '../MainListPage/MainListPage.module.css'
 
 export default class AccountPage extends React.Component {
     static contextType = AppContext;
@@ -15,6 +17,7 @@ export default class AccountPage extends React.Component {
     state = {
         error: null,
         confirm: false,
+        userGuide: false
     }
     
     renderPms() {
@@ -87,6 +90,10 @@ export default class AccountPage extends React.Component {
         document.getElementById('pm_email').value = ''
         this.setState({ error: null })
     }
+
+    closeInstructions = () => {
+        this.setState({ userGuide: false })
+    }
     
     render() {
         return (
@@ -99,6 +106,7 @@ export default class AccountPage extends React.Component {
                             <i className={`fas fa-user-circle ${styles.icon}`}></i>
                             <p className={styles.name}>{this.context.user.full_name}</p>
                             <p className={styles.email}>{this.context.user.email}</p>
+                            <Button onClick={() => this.setState({ userGuide: true })}>User Guide</Button>
                         </section>
                         <section className={`${styles.pmSettings} ${styles.accountContainer}`}>
                             <h2>PM Settings</h2>
@@ -119,6 +127,31 @@ export default class AccountPage extends React.Component {
                             </form>
                         </section>
                     </div>
+                    {this.state.userGuide ? (<div>
+                        <div className={`${modalStyles.formContainer} ${styles.guideContainer}`}>
+                            <button onClick={this.closeInstructions}  className={modalStyles.xButton}><i className="fas fa-times"></i></button>
+                            <div className={styles.instructionContent}>
+                                <h3 className={modalStyles.title}>User Guide</h3>
+                                <div className={`${listStyles.instructions}`}>
+                                    <p>Welcome to the coordinator tool! Here's how to get started:</p>
+                                    <ol>
+                                        <li>Add your Project Manager names and emails in the <span className={listStyles.tabStyle}><i className="fas fa-user-circle"></i> Account</span> tab PM Settings section.</li>
+                                        <li>Create some email templates in the <span className={listStyles.tabStyle}><i className="fas fa-envelope"></i> Templates</span> tab.</li>
+                                        <li>Add your first list item! Use the <span className={listStyles.addButtonStyle}>+ Add Item</span> button to start adding list items. You can include URL's for the CRM advisor or project page which will link the list item values to the external pages for quick navigation in the future.</li>
+                                        <li>Other tips:</li>
+                                            <ul>
+                                                <li>Use the <span className={listStyles.buttonStyle}>PM Update</span> button to automatically generate an email with all of your current list items categorized by PM. This can be used to send an update email to all of your PM's whenever you need to.</li>
+                                                <li>The <span className={listStyles.buttonStyle}>Reset</span> button can be used to reset the status of all of the list items back to no status. You can use this in the beginning of the day to mark all items as no longer pending from the previous day.</li>
+                                                <li>When you mark a list item completed, it will automatically get moved into the <span className={listStyles.tabStyle}><i className="fas fa-check-square"></i> Completed</span> tab where you can revert the status of any completed item if needed and move it back into your main list.</li>
+                                                <li>Use the <span className={listStyles.tabStyle}><i className="fas fa-chart-pie"></i> Dashboard</span> tab to keep track of metrics and gain insight into trends related to your workflow.</li>
+                                            </ul>
+                                    </ol>
+                                </div>
+                            </div>
+                            
+                        </div>
+                        <div className={modalStyles.overlay} onClick={this.closeInstructions}></div>
+                    </div>) : ''}
                 </main>
                 <NavBar className="nav" />
             </div>
